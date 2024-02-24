5475d37344a9c92216f5242a11741cca7d33dbd3
SharkSmile <44153946+sahaswin@users.noreply.github.com>
2024-02-23 20:53:35 -0500

    Updated DataSplit.py

--- EDA Notebooks/Saraka/DataSplit.py
+++ EDA Notebooks/Saraka/DataSplit.py
@@ -6,9 +6,9 @@ from imblearn.under_sampling import RandomUnderSampler
 
 
 def dtreformat(df):
-    df = df.drop((['customerID', 'gender', 'PhoneService', 'MultipleLines', 'InternetService']), axis=1)
-    #df = df.drop((['customerID', 'gender', 'PhoneService', 'MultipleLines', 'InternetService', 'StreamingTV',
-    #               'StreamingMovies']), axis=1)
+    # df = df.drop((['customerID', 'gender', 'PhoneService', 'MultipleLines', 'InternetService']), axis=1)
+    df = df.drop((['customerID', 'gender', 'PhoneService', 'MultipleLines', 'InternetService', 'StreamingTV',
+                   'StreamingMovies']), axis=1)
     df['TotalCharges'] = df['TotalCharges'].replace({' ': 0})
     df['TotalCharges'] = pd.to_numeric(df['TotalCharges'])
     numeric_cols = df._get_numeric_data().columns
@@ -24,21 +24,17 @@ def datasplit(df, target):
     X = df.drop(columns=[target])
     Y = df[target]
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=10)
-    print(X_train.shape)
-    print(X_test.shape)
-    print(Y_train.shape)
-    print(Y_test.shape)
 
     return X_train, X_test, Y_train, Y_test
 
 
-def smote(X, y):
+def smote(x, y):
     smote = SMOTE(random_state=42)
-    X_oversampled, y_oversampled = smote.fit_resample(X, y)
+    X_oversampled, y_oversampled = smote.fit_resample(x, y)
     return X_oversampled, y_oversampled
 
 
-def RUS(X, y):
+def RUS(x, y):
     undersampler = RandomUnderSampler(random_state=42)
-    X_undersampled, y_undersampled = undersampler.fit_resample(X, y)
+    X_undersampled, y_undersampled = undersampler.fit_resample(x, y)
     return X_undersampled, y_undersampled
